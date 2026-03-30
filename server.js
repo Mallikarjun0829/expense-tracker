0425046077fb6270aa22088e5ed79b68d540e89f
Mallikarjun0829 <MALLIKARJUNMADDI298@GMAIL.COM>
2026-03-30 20:09:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,10 +30,11 @@ app.listen(PORT, () => {
 
 // DB Connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "expense_tracker"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
